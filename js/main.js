da17cc7ee7b35016580b95e32041559b3d835187
suzuki.sh <s2s@live.jp>
2018-04-20 16:35:15 +0900

    add save function

--- js/main.js
+++ js/main.js
@@ -1,14 +1,40 @@
 require.config({ paths: { 'vs': 'node_modules/monaco-editor/min/vs' }});
 require(['vs/editor/editor.main'], function() {
-  var editor = monaco.editor.create(document.getElementById('container'), {
-    value: [
-      'function x() {',
-      '\tconsole.log("Hello world!");',
-      '}'
-    ].join('\n'),
+  const storageKey = "monaco-on-cloud-saved";
+  const editor = monaco.editor.create(document.getElementById('editor'), {
+    value: localStorage.getItem(storageKey),
     theme: 'vs-dark',
     language: 'javascript'
   });
+  document.getElementById("save").disabled = true;
+  document.getElementById("save").onclick = function() {
+    save();
+  };
+  document.getElementById("load").onclick = function() {
+    load();
+  };
+  document.addEventListener("keydown", function(e) {
+    // ctrl + s
+    if (e.keyCode == 83 && (navigator.platform.match("Mac") ? e.metaKey : e.ctrlKey)) {
+      e.preventDefault();
+      save();
+    }
+  });
+  editor.onDidChangeModelContent(function(e) {
+    document.getElementById("save").disabled = false;
+  });
+
+  function save() {
+    localStorage.setItem(storageKey, editor.getValue());
+    document.getElementById("save").disabled = true;
+  }
+  function load() {
+    if (window.confirm("Are you sure you want to load data?")) {
+      editor.setValue(localStorage.getItem(storageKey));
+      document.getElementById("save").disabled = true;
+    }
+  }
+
 });
 
 if ('serviceWorker' in navigator) {
